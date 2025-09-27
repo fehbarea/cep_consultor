fede9991cb6222ffe6f6cf105b2ce9c2d0c90f66
Daniel Jacob <danieljaroszczuk28@gmail.com>
2025-09-27 14:32:18 -0300

    feat:Criação do header e implementação do lazy no formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import { Provider } from 'react-redux';
-import FormEndereco from './components/FormEndereco/FormEndereco.js';
+import React, { Suspense } from 'react';
 import store from './contexts/store';
+import Header from './components/Header/Header.js';
+
+const FormEndereco = React.lazy(() => import('./components/FormEndereco/FormEndereco.js'));
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <FormEndereco/>
+        <Header />
+        <br></br>
+        <Suspense fallback={<div>Carregando Formulário...</div>}>
+          <FormEndereco />
+        </Suspense>
       </div>
     </Provider>
   );
